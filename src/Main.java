e4d23bfe2d1309eadf3d9770f54349ac7c756910
Anderson lima <and.seijuro21@gmail.com>
2024-12-16 23:42:36 -0300

    feat: subindo branch 3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import br.alura.aula.objeto.Obj;
+import br.alura.aula.objeto.Obj2;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,5 +26,17 @@ public class Main {
         System.out.println("Total de valores enviados: " + objeto1.getTotalDeValoresEnviados());
 
         System.out.println(objeto1.retornaMedia());
+
+        Obj2 objeto2 = new Obj2();
+
+        objeto2.setValues("Segundo obj");
+        objeto2.setValueInt(2);
+        objeto2.setValueDouble(20);
+        objeto2.exibeObj();
+        objeto2.somaValores(6);
+        objeto2.somaValores(20);
+        System.out.println("Total de valores enviados OBJETO 2: " + objeto2.getTotalDeValoresEnviados());
+        System.out.println(objeto2.retornaMedia());
+
     }
 }
---
+++ src/br/alura/aula/objeto/Obj2.java
@@ -0,0 +1,13 @@
+package br.alura.aula.objeto;
+
+public class Obj2 extends Heranca{
+    String obj2;
+
+    public String getObj2() {
+        return obj2;
+    }
+
+    public void setObj2(String obj2) {
+        this.obj2 = obj2;
+    }
+}
